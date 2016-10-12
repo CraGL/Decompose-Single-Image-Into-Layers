dbe1e8699910aee0d9e113d49eb73410f205c8c6
jianchao <tanjianchaoustc@gmail.com>
2016-10-12 00:04:54 -0400

    add one more example: moth

---
+++ examples/moth-06-ASAP.js
@@ -0,0 +1,7 @@
+{
+    "input_image_name": "moth.png",
+    "input_vertices_file_name": "moth-final_simplified_hull_clip-06.js",
+    "input_vertices_order_file_name": "moth-06-vertex_order.js",
+    "output_prefix": "moth-06-layers-ASAP",
+    "weights": { "w_polynomial": 375.0, "w_opaque": 1.0 }
+}
